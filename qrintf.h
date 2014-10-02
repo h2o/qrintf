6c9110b32900d649c392acc96bed57f8634f4df0
Kazuho Oku <kazuhooku@gmail.com>
2014-10-03 05:47:40 +0900

    support %x

--- qrintf.h
+++ qrintf.h
@@ -75,30 +75,6 @@ static inline qrintf_t _qrintf_s_len(qrintf_t ctx, const char *s, size_t l)
     return ctx;
 }
 
-#define _QP_UNSIGNED_F(type, suffix, max) \
-    static inline qrintf_t _qrintf_ ## suffix (qrintf_t ctx, type v) \
-    { \
-        char tmp[sizeof(QP_TOSTR(max)) - 1]; \
-        size_t i = 0; \
-        if (v == 0) { \
-            ctx.str[ctx.off++] = '0'; \
-            return ctx; \
-        } \
-        do { \
-            tmp[i++] = '0' + v % 10; \
-        } while ((v /= 10) != 0); \
-        do { \
-            ctx.str[ctx.off++] = tmp[--i]; \
-        } while (i != 0); \
-        return ctx; \
-    }
-
-_QP_UNSIGNED_F(unsigned short, hu, USHRT_MAX)
-_QP_UNSIGNED_F(unsigned, u, UINT_MAX)
-_QP_UNSIGNED_F(unsigned long, lu, ULONG_MAX)
-_QP_UNSIGNED_F(unsigned long long, llu, ULONGLONG_MAX)
-_QP_UNSIGNED_F(size_t, zu, SIZE_MAX)
-
 #define _QP_SIGNED_F(type, suffix, min, max) \
     static inline qrintf_t _qrintf_ ## suffix (qrintf_t ctx, type v) \
     { \
@@ -127,6 +103,59 @@ _QP_SIGNED_F(short, hd, SHRT_MIN, SHRT_MAX)
 _QP_SIGNED_F(int, d, INT_MIN, INT_MAX)
 _QP_SIGNED_F(long, ld, LONG_MIN, LONG_MAX)
 _QP_SIGNED_F(long long, lld, LLONG_MIN, LLONG_MAX)
+
+#define _QP_UNSIGNED_F(type, suffix, max) \
+    static inline qrintf_t _qrintf_ ## suffix (qrintf_t ctx, type v) \
+    { \
+        char tmp[sizeof(QP_TOSTR(max)) - 1]; \
+        size_t i = 0; \
+        if (v == 0) { \
+            ctx.str[ctx.off++] = '0'; \
+            return ctx; \
+        } \
+        do { \
+            tmp[i++] = '0' + v % 10; \
+        } while ((v /= 10) != 0); \
+        do { \
+            ctx.str[ctx.off++] = tmp[--i]; \
+        } while (i != 0); \
+        return ctx; \
+    }
+
+_QP_UNSIGNED_F(unsigned short, hu, USHRT_MAX)
+_QP_UNSIGNED_F(unsigned, u, UINT_MAX)
+_QP_UNSIGNED_F(unsigned long, lu, ULONG_MAX)
+_QP_UNSIGNED_F(unsigned long long, llu, ULLONG_MAX)
+_QP_UNSIGNED_F(size_t, zu, SIZE_MAX)
+
+#define _QP_HEX_F(type, suffix, uc) \
+    static inline qrintf_t _qrintf_ ## suffix (qrintf_t ctx, type v) \
+    { \
+        char tmp[sizeof(type) * 2]; \
+        size_t i = 0; \
+        if (v == 0) { \
+            ctx.str[ctx.off++] = '0'; \
+            return ctx; \
+        } \
+        do { \
+            tmp[i++] = (uc ? "0123456789ABCDEF" : "0123456789abcdef")[v & 0xf]; \
+        } while ((v >>= 4) != 0); \
+        do { \
+            ctx.str[ctx.off++] = tmp[--i]; \
+        } while (i != 0); \
+        return ctx; \
+    }
+
+_QP_HEX_F(unsigned short, hx, 0)
+_QP_HEX_F(unsigned short, hX, 1)
+_QP_HEX_F(unsigned, x, 0)
+_QP_HEX_F(unsigned, X, 1)
+_QP_HEX_F(unsigned long, lx, 0)
+_QP_HEX_F(unsigned long, lX, 1)
+_QP_HEX_F(unsigned long long, llx, 0)
+_QP_HEX_F(unsigned long long, llX, 1)
+_QP_HEX_F(size_t, zx, 0)
+_QP_HEX_F(size_t, zX, 1)
 
 #ifdef __cplusplus
 }
